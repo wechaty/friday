a6ca079b3f27ca12f5c2cba5c332675fc763173b
windmemory <wind.memory.cn@gmail.com>
2019-12-20 12:13:31 +0800

    add vote manager to manage vote message in room

--- src/handlers/on-message.ts
+++ src/handlers/on-message.ts
@@ -3,10 +3,16 @@ import {
   Message,
   Wechaty,
 }             from 'wechaty'
+import { VoteManager } from '../managers/voteManager'
 
 export default async function onMessage (
   this    : Wechaty,
   message : Message,
 ): Promise<void> {
   log.info('on-message', 'onMessage(%s)', message)
+  try {
+    await VoteManager.checkVote(message)
+  } catch (e) {
+    log.error('on-message', 'Failed to check vote for the message:\n', e)
+  }
 }
