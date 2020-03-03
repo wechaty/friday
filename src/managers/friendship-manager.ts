6e47826a8618b0db853a44493a1078f45d9ef216
Huan LI <zixia@zixia.net>
2020-03-03 21:45:01 +0800

    hide python room

--- src/managers/friendship-manager.ts
+++ src/managers/friendship-manager.ts
@@ -2,10 +2,10 @@ import { Friendship } from 'wechaty'
 
 const NEW_FRIEND_GREETING = [
   '嗨，你好，我是 Friday BOT。',
-  '目前我在管理的公开群有 3 个，回复关键词进群：',
+  '目前我在管理的公开群有 2 个，回复关键词进群：',
   '----------------------------',
   `wechaty - 加入"Wechaty Developers' Home"`,
-  'python - 加入 "Python Wechaty"',
+  // 'python - 加入 "Python Wechaty"',
   'aidog - 加入“Youth fed the 5th dog”',
 ].join('\n')
 
