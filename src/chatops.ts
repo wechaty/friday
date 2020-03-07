d463544a0b06b3701a525c739cd7722c23a5cbbe
Huan LI <zixia@zixia.net>
2020-03-08 00:34:18 +0800

    enhance chatops & on friendship

--- src/chatops.ts
+++ src/chatops.ts
@@ -4,6 +4,8 @@ import {
   UrlLink,
 }             from 'wechaty'
 
+import { DelayQueueExecutor } from 'rx-queue'
+
 import {
   log,
   BOT_ROOM_ID,
@@ -32,11 +34,12 @@ export class Chatops {
    * --------
    * Instance
    */
+  private delayQueueExecutor: DelayQueueExecutor
 
   private constructor (
     private bot: Wechaty,
   ) {
-    //
+    this.delayQueueExecutor = new DelayQueueExecutor(5 * 1000)  // set delay period time to 5 seconds
   }
 
   public async heartbeat (text: string): Promise<void> {
@@ -102,6 +105,15 @@ export class Chatops {
       await room.ready()
       await room.announce(announcement)
     }
+  }
+
+  public async queue (
+    fn: (() => any),
+    name?: string,
+  ) {
+    log.verbose('Chatops', 'queue(,"%s")', name)
+    await this.delayQueueExecutor.execute(fn, name)
+    log.verbose('Chatops', 'queue(,"%s") done.', name)
   }
 
 }
--- src/handlers/on-friendship.ts
+++ src/handlers/on-friendship.ts
@@ -1,15 +1,34 @@
 import {
-  Friendship,
   log,
+  Friendship,
   Wechaty,
 }             from 'wechaty'
-import { FriendshipManager } from '../managers/friendship-manager'
+
+import { FriendshipManager }  from '../managers/friendship-manager'
+
+import { Chatops }            from '../chatops'
 
 export default async function onFriendship (
   this       : Wechaty,
   friendship : Friendship,
 ): Promise<void> {
   log.info('on-friendship', 'onFriendship(%s)', friendship)
+
+  const contact = await friendship.contact()
+  const hello = await friendship.hello()
+
+  if (friendship.type() === this.Friendship.Type.Receive) {
+    await Chatops.instance().queue(() => {
+      Chatops.instance().say(`recreived friendship from ${contact} with ${hello}`)
+        .catch(e => log.error('on-friendship', 'onFriendship() queue() rejection %s', e))
+    })
+  } else if (friendship.type() === this.Friendship.Type.Confirm) {
+    await Chatops.instance().queue(() => {
+      Chatops.instance().say(`confirmed friendship from ${contact}`)
+        .catch(e => log.error('on-friendship', 'onFriendship() queue() rejection %s', e))
+    })
+  }
+
   try {
     await FriendshipManager.autoProcessFriendship(friendship)
   } catch (e) {
