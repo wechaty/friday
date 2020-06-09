8eb0c10c191f0ec11858d6f5e20fd010941f8cff
Huan LI (李卓桓) <zixia@zixia.net>
2020-06-10 03:22:17 +0800

    VoteOut plugin v0.3

--- src/plugins/vote-out.ts
+++ src/plugins/vote-out.ts
@@ -2,40 +2,21 @@
 import {
   VoteOut,
   VoteOutConfig,
-}                 from 'wechaty-voteout'
+}                             from 'wechaty-voteout'
 
-// import {
-//   DEVELOPERS_ROOM_ID_LIST,
-// }                           from '../rooms-config'
-
-const config: VoteOutConfig = {
+const config: Partial<VoteOutConfig> = {
   // When the people reach the target, then means (s)he has been voted out.
   target: 3,
-  // Warn template, set to falsy to disable the warn message
-  warnTemplate: '可能是因为你的聊天内容不当导致被用户投票，当前票数为 {{ count }}，当天累计票数达到 {{ target }} 时，你将被请出此群。',
-  // Kickout template, set to falsy to disable the message.
-  kickoutTemplate: '经 {{ voters }} 几人投票，你即将离开此群。',
-  // Different puppet get different sign
-  // We run more cases to see what sign it is, and update the comment here.
-  sign: [
-    '[弱]',
-    '[ThumbsDown]',
-    '<img class="qqemoji qqemoji80" text="[弱]_web" src="/zh_CN/htmledition/v2/images/spacer.gif" />',
+  warnTemplate: 'You have been warned {{ count }} times，you will be removed if you receive {{ target }} consecutive warnings.',
+  kickoutTemplate: 'You have been voted to be an unwelcome guest in this room by {{ voters }}, kicking you out is a significant notice for everyone who is still in this room be nice, bye-bye...',
+  room: [
+    /Wechaty Developers/i,
+    /Youth fed the/i,
   ],
-
-  // The function to check if some one is voted.
-  // Default function is to check is there a sign in the text.
-  isVoted: null,
-  // Which room(s) you want the bot to work with.
-  // Can be a room topic array or a function
-  // E.g. ['Room1', 'Room2']
-  // E.g. room: function (room) { room.topic().indexOf('我的') > -1 }
-  // Set to falsy value means works for all rooms.
-  room: false,
   // Who never be kickedout by voting
-  whiteList: [],
-  // Vote expred time, default to 1 day.
-  expired: 24 * 3600 * 1000, // 1 day
+  whiteList: [
+    'lizhuohuan',
+  ],
 }
 
 export const VoteOutPlugin = VoteOut(config)
