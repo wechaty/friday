e73f58bf850a304c6f9230ae0750c59569478bdd
Huan LI (李卓桓) <zixia@zixia.net>
2020-06-20 16:49:19 +0800

    add vorpal contrib: ding & eval

--- src/plugins/vorpal.ts
+++ src/plugins/vorpal.ts
@@ -2,6 +2,11 @@ import {
   WechatyVorpal,
   WechatyVorpalConfig,
 }                         from 'wechaty-vorpal'
+import {
+  Ding,
+  Eval,
+}                         from 'wechaty-vorpal-contrib'
+
 import { CHATOPS_ROOM_ID } from '../id-config'
 
 const hackerNews = require('vorpal-hacker-news')
@@ -12,10 +17,13 @@ const extensionList = [
    *  hacker-news --length 3
    */
   hackerNews,
+  Ding(),
+  Eval(),
 ]
+
 const config: WechatyVorpalConfig = {
-  room: CHATOPS_ROOM_ID,
-  use: extensionList,
+  room : CHATOPS_ROOM_ID,
+  use  : extensionList,
 }
 
 export const VorpalPlugin = WechatyVorpal(config)
