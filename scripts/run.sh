be707ee03a1bf17541b3106b88c6e5ca7074c79b
Huan LI (李卓桓) <zixia@zixia.net>
2020-07-15 04:32:12 +0800

    do not exit when the script exit with error

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-set -e
-
 LOG_FILE=friday.log
 
-while true
-  do git checkout .
+while true do
+  git checkout .
   git pull
   rm -f package-lock.json
   npm i
   npm run build
-  node dist/src/main.js | tee "$LOG_FILE" || true
+  node dist/src/main.js | tee "$LOG_FILE"
   sleep 1
 done
