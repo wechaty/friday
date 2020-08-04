03b0971702d3ba93ea8debcdb2ffc1b3dee2896f
Huan LI (李卓桓) <zixia@zixia.net>
2020-08-04 14:50:04 +0800

    fix fixtures

--- tests/fixtures/smoke-testing.ts
+++ tests/fixtures/smoke-testing.ts
@@ -1,14 +1,11 @@
-import { getWechaty } from '../../src/wechaty/mod'
-import { setupWeb }   from '../../src/web/mod'
+import { getFriday } from '../../src/friday/bot'
 
 process.env.WECHATY_PUPPET = 'wechaty-puppet-mock'
 
 async function main () {
-  const bot = getWechaty('smoke-testing')
+  const bot = getFriday('smoke-testing')
 
   await bot.start()
-  await setupWeb(bot)
-
   await bot.stop()
 
   return 0
