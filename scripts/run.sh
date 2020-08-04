94a8f61f65e4caced3d45c7b440c7de0e772ebf6
Huan LI (李卓桓) <zixia@zixia.net>
2020-08-04 23:34:16 +0800

    support args for run

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,9 +3,12 @@
 set -x
 set -eo pipefail
 
-function runSource () {
+function updateGit () {
   git checkout .
   git pull
+}
+
+function runSource () {
   rm -f package-lock.json
   npm i
   npm run build
@@ -18,11 +21,34 @@ function runDocker () {
 }
 
 function main () {
+
+  case ${1:-docker} in
+    source)
+      RUN_CMD=runSource
+      ;;
+
+    *)
+      ;&
+    docker)
+      RUN_CMD=runDocker
+      ;;
+  esac
+
+case ${2} in
+  update)
+    UPDATE_CMD=updateGit
+    ;;
+  *)
+    UPDATE_CMD=true
+    ;;
+  esac
+
   while true
   do
-    runDocker
+    "$UPDATE_CMD"
+    "$RUN_CMD"
     sleep 1
   done
 }
 
-main
+main "$@"
