d161b128b365b005445f55c2ba6415106a720f4e
Huan LI (李卓桓) <zixia@zixia.net>
2020-12-01 21:54:36 +0800

    more details about room invitation problem. (#62)

--- src/friday/plugins/ding-dong/mod.ts
+++ src/friday/plugins/ding-dong/mod.ts
@@ -10,7 +10,11 @@ const wechatyDingDongConfig = {
     /^wechaty$/i,
     /^(\w+)*\s*wechaty\s*(\w+)*$/i,
   ],
-  dong: "Welcome to join our Wechaty Developers' Home by visiting https://gitter.im/wechaty/wechaty",
+  dong: [
+    "Welcome to join our Wechaty Developers' Home by visiting https://gitter.im/wechaty/wechaty",
+    'Currently, the WeChat room invitation is temporary not available because we are experiencing issue #62, learn more from https://github.com/wechaty/friday/issues/62 if you are interested.',
+    'If you want to join the WeChat room, please come back and send me "Wechaty" again after December 3rd.',
+  ],
 }
 
 const WechatyDingDongPlugin = DingDong({
