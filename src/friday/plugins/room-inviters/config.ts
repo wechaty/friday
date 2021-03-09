2c30284a7174722acf40cc78a674f87dd934ea20
Huan <zixia@zixia.net>
2021-03-09 16:55:43 +0800

    add polyglot docs link

--- src/friday/plugins/room-inviters/config.ts
+++ src/friday/plugins/room-inviters/config.ts
@@ -17,7 +17,9 @@ const WECHATY_DEVELOPERS_ROOM_RULES = [
   'Please introduce yourself after you join the room, cheers!',
 ]
 const WECHATY_DEVELOPERS_ROOM_WELCOME = [
-  ", welcome to join the Wechaty Python/Go/Java/PHP/.Net/Scala/Rust Developer's Home! Please go ahead to introduce yourself to the group.",
+  ", welcome to join the Wechaty Python/Go/Java/PHP/.Net/Scala/Rust Developer's Home!",
+  'You can find Polyglot Wechaty docs at https://wechaty.js.org/docs/polyglot/ ,',
+  'Please go ahead to introduce yourself to the group.',
 ]
 
 const WECHATY_DEVELOPERS_ROOM_RULES_CHINESE = [
