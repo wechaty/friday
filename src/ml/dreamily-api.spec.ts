1f12c819634b005c72940dff05c4875f47f20e28
Huan <zixia@zixia.net>
2021-09-12 16:32:28 +0800

    remove dreamily unit test

--- src/ml/dreamily-api.spec.ts
+++ src/ml/dreamily-api.spec.ts
@@ -7,7 +7,7 @@ import {
   dreamilyApi,
 }                   from './dreamily-api.js'
 
-test('Dreamily API', async (t) => {
+test.skip('Dreamily API', async (t) => {
   const PAYLOAD = {
     content: '不想当将军的司机不是好厨子',
     style: StoryStyle.imaginative,
