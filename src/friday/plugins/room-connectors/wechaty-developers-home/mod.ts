ef38d7ca45c9fc4cfcc61c4f9c328f28ad01d06d
Huan <zixia@zixia.net>
2021-09-12 21:44:40 +0800

    clean

--- src/friday/plugins/room-connectors/wechaty-developers-home/mod.ts
+++ src/friday/plugins/room-connectors/wechaty-developers-home/mod.ts
@@ -1,6 +1,6 @@
 export * from './home-mixer.js'
 export * from './home-hq.js'
-export * from './announcement.js'
+export * from './broadcast-station.js'
 export * from './polyglot-broadcast.js'
 
 export * from './language-filtered/mod.js'
