ec56213af3128b0bd8bf1981cfd956ac8f9c0157
Huan (李卓桓) <zixia@zixia.net>
2021-11-01 02:01:50 +0800

    add testing room

--- src/friday/plugins/bot5-assistant.ts
+++ src/friday/plugins/bot5-assistant.ts
@@ -5,7 +5,10 @@ import {
 import * as BOT5 from '../../database/bot5.js'
 
 const Bot5AssistantPlugin = Bot5Assistant({
-  room: BOT5.member[0],
+  room: [
+    BOT5.member[0], // room id
+    /^BOT5 /,       // room topic
+  ],
 })
 
 export {
