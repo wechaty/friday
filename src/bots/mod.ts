99162fde34a6bf0433dc7cf7300463180cff10ea
Huan (李卓桓) <zixia@zixia.net>
2022-01-27 23:43:53 +0800

    add whatsapp for Friday BOT (#111)

--- src/bots/mod.ts
+++ src/bots/mod.ts
@@ -3,6 +3,7 @@ import { getWxWork }  from './wxwork/bot.js'
 import { getHuanOa }  from './huan-oa/bot.js'
 import { getGitter }  from './gitter/bot.js'
 import { getQQ }      from './qq/bot.js'
+import { getWhatsapp } from './whatsapp/bot.js'
 
 void getCeibs
 void getWxWork
@@ -19,6 +20,7 @@ const getBots = () => ({
   oa: getHuanOa('Huan.OA'),
   // wxwork : getWxWork('WxWork.BOT'),
   qq: getQQ('QQ'),
+  whatsapp: getWhatsapp('Whatsapp'),
 })
 
 export { getBots }
