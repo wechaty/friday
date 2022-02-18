e814e60289366f7c9fea5c9e0d4e8b64d28491dc
Huan (李卓桓) <zixia@zixia.net>
2022-02-18 10:32:36 +0800

    finish room events saga handlers

---
+++ src/sync-community-rooms-domain/events/handlers/qq-community-message-received.handler.ts
@@ -0,0 +1,70 @@
+import {
+  type IEventHandler,
+  EventsHandler,
+  CommandBus,
+  QueryBus,
+}                         from '@nestjs/cqrs'
+import type { Brolog }    from 'brolog'
+import {
+  ForwardMessageToGitterCommunityCommand,
+  ForwardMessageToWeChatCommunityCommand,
+  ForwardMessageToWhatsAppCommunityCommand,
+  ForwardTextMessageToGitterCommunityCommand,
+  ForwardTextMessageToWeChatCommunityCommand,
+  ForwardTextMessageToWhatsAppCommunityCommand,
+}                                                   from '../../commands/mod.js'
+import { IsMessageTypeTextQuery }                   from '../../queries/mod.js'
+
+import {
+  QqCommunityMessageReceivedEvent,
+}                                       from '../mod.js'
+
+@EventsHandler(QqCommunityMessageReceivedEvent)
+export class QqCommunityMessageReceivedHandler implements IEventHandler<QqCommunityMessageReceivedEvent> {
+
+  constructor (
+    private readonly log: Brolog,
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
+
+  async handle (event: QqCommunityMessageReceivedEvent) {
+    this.log.verbose('QqCommunityMessageReceivedHandler', 'handle({puppetId: %s, messageId: %s})', event.puppetId, event.messageId)
+
+    const isTypeText: boolean = await this.queryBus.execute(
+      new IsMessageTypeTextQuery(
+        event.puppetId,
+        event.messageId,
+      ),
+    )
+
+    const commandClassList = []
+
+    if (isTypeText) {
+      commandClassList.push(
+        ForwardTextMessageToGitterCommunityCommand,
+        ForwardTextMessageToWeChatCommunityCommand,
+        ForwardTextMessageToWhatsAppCommunityCommand,
+      )
+    } else {
+      commandClassList.push(
+        ForwardMessageToGitterCommunityCommand,
+        ForwardMessageToWeChatCommunityCommand,
+        ForwardMessageToWhatsAppCommunityCommand,
+      )
+    }
+
+    /**
+     * Bravo! the `commandClassList` has static typing for all types that pushed earlier!
+     */
+    commandClassList.forEach(Command =>
+      this.commandBus.execute(
+        new Command(
+          event.puppetId,
+          event.messageId,
+        ),
+      ),
+    )
+  }
+
+}
---
+++ src/sync-community-rooms-domain/events/handlers/wechat-community-message-received.handler.ts
@@ -0,0 +1,70 @@
+import {
+  type IEventHandler,
+  EventsHandler,
+  CommandBus,
+  QueryBus,
+}                         from '@nestjs/cqrs'
+import type { Brolog }    from 'brolog'
+import {
+  ForwardMessageToGitterCommunityCommand,
+  ForwardMessageToQqCommunityCommand,
+  ForwardMessageToWhatsAppCommunityCommand,
+  ForwardTextMessageToGitterCommunityCommand,
+  ForwardTextMessageToQqCommunityCommand,
+  ForwardTextMessageToWhatsAppCommunityCommand,
+}                                                   from '../../commands/mod.js'
+import { IsMessageTypeTextQuery } from '../../queries/mod.js';
+
+import {
+  WeChatCommunityMessageReceivedEvent,
+}                                       from '../mod.js'
+
+@EventsHandler(WeChatCommunityMessageReceivedEvent)
+export class WeChatCommunityMessageReceivedHandler implements IEventHandler<WeChatCommunityMessageReceivedEvent> {
+
+  constructor (
+    private readonly log: Brolog,
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
+
+  async handle (event: WeChatCommunityMessageReceivedEvent) {
+    this.log.verbose('WeChatCommunityMessageReceivedHandler', 'handle({puppetId: %s, messageId: %s})', event.puppetId, event.messageId)
+
+    const isTypeText: boolean = await this.queryBus.execute(
+      new IsMessageTypeTextQuery(
+        event.puppetId,
+        event.messageId,
+      ),
+    )
+
+    const commandClassList = []
+
+    if (isTypeText) {
+      commandClassList.push(
+        ForwardTextMessageToGitterCommunityCommand,
+        ForwardTextMessageToQqCommunityCommand,
+        ForwardTextMessageToWhatsAppCommunityCommand,
+      )
+    } else {
+      commandClassList.push(
+        ForwardMessageToGitterCommunityCommand,
+        ForwardMessageToQqCommunityCommand,
+        ForwardMessageToWhatsAppCommunityCommand,
+      )
+    }
+
+    /**
+     * Bravo! the `commandClassList` has static typing for all types that pushed earlier!
+     */
+    commandClassList.forEach(Command =>
+      this.commandBus.execute(
+        new Command(
+          event.puppetId,
+          event.messageId,
+        ),
+      ),
+    )
+  }
+
+}
