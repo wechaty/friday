f697295bb308e580cedebb88931a9920ec5396a6
Huan (李卓桓) <zixia@zixia.net>
2022-02-23 15:45:54 +0800

    use fetch for statuspage api

--- src/core/statuspage/statuspage.service.ts
+++ src/core/statuspage/statuspage.service.ts
@@ -1,5 +1,6 @@
-import https from 'https'
 import { Injectable } from '@nestjs/common'
+import { Brolog }     from 'brolog'
+import fetch          from 'cross-fetch'
 
 import { StatuspageSettings } from './statuspage.settings.js'
 
@@ -7,7 +8,14 @@ import { StatuspageSettings } from './statuspage.settings.js'
  * Atlassian's status page metric submitter
  *  https://statuspage.io
  */
-const statuspageMetricSubmitter = (apiKey: string, pageId: string, metricId: string) => {
+const statuspageMetricSubmitter = (
+  log: Brolog,
+  apiKey: string,
+  pageId: string,
+  metricId: string,
+) => {
+  log.verbose('StatuspageService', 'statuspageMetricSubmitter() metricId=%s', metricId)
+
   const apiBase = 'https://api.statuspage.io/v1'
 
   const url = apiBase + '/pages/' + pageId + '/metrics/' + metricId + '/data.json'
@@ -20,6 +28,7 @@ const statuspageMetricSubmitter = (apiKey: string, pageId: string, metricId: str
   }
 
   return async function submit (value: number) {
+    log.verbose('StatuspageService', 'statuspageMetricSubmitter() submit(%d) metricId=%s', value, metricId)
     const timestamp = Math.floor(Date.now() / 1000)
 
     const data = {
@@ -27,18 +36,31 @@ const statuspageMetricSubmitter = (apiKey: string, pageId: string, metricId: str
       value,
     }
 
-    return new Promise<void>((resolve, reject) => {
-      const request = https.request(url, requestOptions, function (res) {
-        if (res.statusMessage === 'Unauthorized') {
-          const error = new Error('Please ensure that your page code and authorization key are correct.')
-          return reject(error)
-        }
-        res.on('error', reject)
-        res.on('data', resolve)
-      })
-      const str = JSON.stringify({ data })
-      request.end(str)
+    const response = await fetch(url, {
+      ...requestOptions,
+      body: JSON.stringify({ data }),
     })
+
+    console.info(new Error().stack)
+
+    if (response.statusText === 'Unauthorized') {
+      log.error('Please ensure that your page code and authorization key are correct.')
+      return
+    }
+    return response.json()
+
+    // return new Promise<void>((resolve, reject) => {
+    //   const request = https.request(url, requestOptions, function (res) {
+    //     if (res.statusMessage === 'Unauthorized') {
+    //       const error = new Error('Please ensure that your page code and authorization key are correct.')
+    //       return reject(error)
+    //     }
+    //     res.on('error', reject)
+    //     res.on('data', resolve)
+    //   })
+    //   const str = JSON.stringify({ data })
+    //   request.end(str)
+    // })
   }
 }
 
@@ -51,18 +73,22 @@ export class StatuspageService {
 
   constructor (
     private readonly settings: StatuspageSettings,
+    readonly log: Brolog,
   ) {
     this.submitMobileTerminatedMessageCount = statuspageMetricSubmitter(
+      log,
       this.settings.apiKey,
       this.settings.pageId,
       this.settings.receivedMessages,
     )
     this.submitMobileOriginatedMessageCount = statuspageMetricSubmitter(
+      log,
       this.settings.apiKey,
       this.settings.pageId,
       this.settings.sentMessages,
     )
     this.submitCommunityMemberCount = statuspageMetricSubmitter(
+      log,
       this.settings.apiKey,
       this.settings.pageId,
       this.settings.members,
