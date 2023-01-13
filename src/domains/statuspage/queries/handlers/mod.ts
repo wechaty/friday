3b26678831d1516c6740bdd843b790bf0b95a945
Huan (李卓桓) <zixia@zixia.net>
2023-01-13 11:12:39 +0800

    provide handler for injection

--- src/domains/statuspage/queries/handlers/mod.ts
+++ src/domains/statuspage/queries/handlers/mod.ts
@@ -1,11 +1,13 @@
 import { GetGitterMembersCountHandler }   from './get-gitter-members-count.handler.js'
 import { GetQqMembersCountHandler }       from './get-qq-members-count.handler.js'
 import { GetWeChatMembersCountHandler }   from './get-wechat-members-count.handler.js'
+import { GetWorkProMembersCountHandler }  from './get-workpro-members-count.handler.js'
 import { GetWhatsAppMembersCountHandler } from './get-whatsapp-members-count.handler.js'
 
 export const QueryHandlers = [
   GetGitterMembersCountHandler,
   GetQqMembersCountHandler,
   GetWeChatMembersCountHandler,
+  GetWorkProMembersCountHandler,
   GetWhatsAppMembersCountHandler,
 ]
